2bdc4bbce6e4aab6626f0a065d020256cdf5b8c1
Richard Wise <richard.wise@hey.com>
2023-12-24 15:27:37 -0500

    update standard to ignore single quote errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -9,12 +11,12 @@
 MEALS = %w[breakfast lunch dinner dessert snack].freeze
 SOURCES = %w[makayla_thomas_fit jasonalexander.fit].freeze
 
-DINNER_RECIPES = ['PARMESAN CRUSTED CHICKEN', "creamy marinara
-chicken bowl", 'rasta pasta bowls', "lemon shrimp
-alfredo bowls", "million dollar
-spaghetti bowl", "5 cheese chicken
-penne bowls", "philly cheesesteak
-pasta bowls"].freeze
+DINNER_RECIPES = ['parmesan crusted chicken', 'creamy marinara
+chicken bowl', 'rasta pasta bowls', 'lemon shrimp
+alfredo bowls', 'million dollar
+spaghetti bowl', '5 cheese chicken
+penne bowls', 'philly cheesesteak
+pasta bowls'].freeze
 
 MEALS.each do |meal|
   Meal.create! name: meal
@@ -30,7 +32,7 @@ DINNER_RECIPES.each do |recipe|
   Recipe.create! name: recipe, source_id: source.id
 end
 
-Recipe.create! name: "Brisket Breakfast Taco", source_id: 2
+Recipe.create! name: 'Brisket Breakfast Taco', source_id: 2
 
 Recipe.last.meals << Meal.first
 
